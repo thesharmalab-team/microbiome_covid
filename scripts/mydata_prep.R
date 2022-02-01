f32da40f98461c97fcfa5a734ca2a77eaa0d13ed
Giorgia Gentili <giorgiaisabella@gentili.co.uk>
2022-02-01 16:34:47 +0100

    adjusted functions to covid

--- scripts/mydata_prep.R
+++ scripts/mydata_prep.R
@@ -1,65 +1,30 @@
 mydata_prep <- function(mypseq) {
 
   # this accepts the list from the pseq_prep_2
-  message("The pseq being used are: \n atlas.recode.country.pseq \n curat.recode.country.pseq ")  
-  atlascountry <- as.data.frame(table(get_variable(mypseq[["atlas.recode.country.pseq"]], "country")))
-  curat2atlas <- as.data.frame(table(get_variable( mypseq[["curat.recode.country.pseq"]], "curat2atlas")))
+  message("The pseq being used are: \n curat.recode.country.pseq ")  
   curatcountry <- as.data.frame(table(get_variable( mypseq[["curat.recode.country.pseq"]], "curatcountry")))
-
-  
-  #mnd.prev.sub$iso3c.location <- countrycode(mnd.prev.sub$location , origin = 'country.name', destination = "iso3c")
-  
-  ####### this labels the curated countries with the atlas labels as well. Important that it uses the ...iso3... codes  mnd.gbd.sub
-  #curatcountry$location <- countrycode(curatcountry$Var1 , origin = "iso3c", destination = 'country.name' )
-  curatcountry <- curatcountry %>% dplyr::rename(curat.label = Var1 )
-  curatcountry$atlas.label <- curatcountry$Freq
-  curatcountry$atlas.label[curatcountry$curat.label %in% curat2atlas.scand.iso3c] <- "Scandinavia"
-  curatcountry$atlas.label[curatcountry$curat.label %in% curat2atlas.ce.iso3c] <- "CentralEurope"
-  curatcountry$atlas.label[curatcountry$curat.label %in% curat2atlas.se.iso3c] <- "SouthEurope"
-  curatcountry$atlas.label[curatcountry$curat.label %in% curat2atlas.uk.iso3c] <- "UKIE"
-  curatcountry$atlas.label[curatcountry$curat.label %in% curat2atlas.us.iso3c] <- "US"
-  # Difficult here is Aut and LUX.
-  curatcountry$atlas.label[curatcountry$curat.label =="AUT"] <- "CentralEurope"
-  curatcountry$atlas.label[curatcountry$curat.label =="LUX"] <- "CentralEurope"
-  names(curatcountry)[1] <- "curat.label"
-  #############################
-  
-  #####################
-  # important to subset the data else it adds all of the ages up together
-  a <- aggregate(DALY~Region+year, subset(mnd.gbd.sub, age == gbd.age), mean)
-  b <- aggregate(Incidence~Region+year, subset(mnd.gbd.sub, age == gbd.age), mean)
-  c <- aggregate(Prevalence~Region+year, subset(mnd.gbd.sub, age == gbd.age), mean)
-  
-  mnd.gbd.gg <- left_join(a, b) %>%
-    left_join(.,c ) 
   
   #this add the number of microbiome for each group
   # renames the columns so that they match
-  atlascountry <- atlascountry %>% dplyr::rename(atlas.freq = Freq, Region = Var1 )
-  mnd.gbd.gg <- left_join(mnd.gbd.gg, atlascountry)
+  curatcountry <- curatcountry %>% dplyr::rename(curat.freq = Freq, country = Var1 )
+  curat.freq <- curatcountry$curat.freq
+  covid.owid.gg <- cbind(covid.owid.gg, curat.freq)
   
-  curat2atlas <- curat2atlas %>% dplyr::rename(Region = Var1,curat.atlas.freq = Freq )
-  mnd.gbd.gg <- left_join(mnd.gbd.gg, curat2atlas)
+  covid.owid.gg <- cbind(covid.owid.gg, curat.freq)
   #####################
-  atlas.curat.combined.agestd <- mnd.gbd.gg
+  curat.combined <- covid.owid.gg
   
   ###### add the frequency to the GBD data
-  curatcountry$atlas.label <- NULL
+  #curatcountry$atlas.label <- NULL
   # renames them so that the column match
-  mnd.gbd.sub$curat.label <- mnd.gbd.sub$iso3c.location
-  curat.combined <- left_join(mnd.gbd.sub, curatcountry)
-  # adds the Atlas samples numbers 
+  #mnd.gbd.sub$curat.label <- mnd.gbd.sub$iso3c.location
+  #curat.combined <- left_join(mnd.gbd.sub, curatcountry)
+ 
+  # OUTPUT ; curat.combined
   
-  curat.combined <- left_join(curat.combined, curat2atlas)
-  # only select the age std data
-  curat.combined.agestd <- subset(curat.combined, age==gbd.age)
-  
-  # OUTPUT ; curat.combined.agestd & atlas.curat.combined.agestd
-  
-  mydata <- list("atlas.curat.combined.agestd" = atlas.curat.combined.agestd, 
-                 "curat.combined.agestd" = curat.combined.agestd,
+  mydata <- list(
                  "curat.combined" = curat.combined)
-  message("Returning mydata that contains:\n atlas.curat.combined.agestd \n curat.combined.agestd \n curat.combined ")
+  message("Returning mydata that contains:\n curat.combined ")
   
   return(mydata)
 
--- scripts/pseq_filter.R
+++ scripts/pseq_filter.R
@@ -3,89 +3,63 @@ pseq_filter <- function(mypseq) {
 # N Sharma 15/9/20
 # This script filters the pseq 
 #  
-# The output is; 
-# atlas.pseq.comp.filter
-# curat.pseq.comp.filte
-
-# Relative abundance
-curat.recode.country.comp.pseq <- microbiome::transform(mypseq[["curat.recode.country.pseq" ]], "compositional")
-
-  message("Coverting the data to relative abudance")
-  
-# Filtering based on filter.features for siamcat
-# siamcat <- filter.features(siamcat, cutoff=1e-04, filter.method = 'abundance', feature.type = 'original')
-
-# Filtered such that only OTUs with a mean greater than 1e-05 are kept.
-curat.recode.country.comp.filter.pseq = filter_taxa(curat.recode.country.comp.pseq , function(x) mean(x) > 1e-04, TRUE)
-
-#Relative population frequencies; at 1% compositional abundance threshold:
-#prevalence(pseq.rel, detection = 1/100, sort = TRUE)
-
-
-
-# the filtering is really important about relative and counts. Sdome are appropriate. Other are not. 
-# # Core
-# keepotu = genefilter_sample(curat.recode.country.comp.pseq, filterfun_sample(topp(0.05)), A=5)
-# pseq.f <- subset_taxa(curat.recode.country.comp.pseq, keepotu)
-# pseq.f = subset_taxa(pseq.f, !is.na(Genus))
-# plot_heatmap(pseq.f, method="PCoA", distance="bray")
-# 
-# pseq.filt = subset_taxa(curat.recode.country.comp.pseq, keepotu & !is.na(Genus))
-# plot_heatmap(pseq.filt, method="PCoA", distance="bray")
-# 
-# sum(is.na(otu_table(physeqITS_DADA)))
-# psmelt(physeqITS_DADA) %>%
-#   filter(is.na(Abundance))
-
-curat.recode.country.comp.core.pseq <- core(curat.recode.country.comp.pseq, detection = 0, prevalence = .5)
-
-# Absolute population frequencies (sample count):
-#prevalence(pseq.rel, detection = 1/100, sort = TRUE, count = TRUE)
-
-curat.recode.country.filter.pseq = filter_taxa(mypseq[["curat.recode.country.pseq"]] , function(x) mean(x) > 1e-04, TRUE)
-curat.recode.country.comp.core.filter.pseq <- core(curat.recode.country.filter.pseq, detection = 0, prevalence = .5)
-
-
-
-#mypseq[["curat.recode.country.comp.core.filter.nNA.pseq"]] <- curat.recode.country.comp.core.filter.nNA.pseq
-
-mypseq[["curat.recode.country.comp.core.filter.pseq"]] <- curat.recode.country.comp.core.filter.pseq
-mypseq[["curat.recode.country.filter.pseq"]] <- curat.recode.country.filter.pseq
-mypseq[["curat.recode.country.comp.filter.pseq"]] <- curat.recode.country.comp.filter.pseq
-mypseq[["curat.recode.country.comp.core.pseq"]] <- curat.recode.country.comp.core.pseq
-mypseq[["curat.recode.country.comp.pseq"]] <- curat.recode.country.comp.pseq
-
-# ATLAS SUBSET
-# Relative abundance
-atlas.recode.country.comp.pseq <- microbiome::transform(mypseq[["atlas.recode.country.pseq" ]], "compositional")
-
-# Filtering based on filter.features for siamcat
-# siamcat <- filter.features(siamcat, cutoff=1e-04, filter.method = 'abundance', feature.type = 'original')
-
-# Filtered such that only OTUs with a mean greater than 1e-05 are kept.
-atlas.recode.country.comp.filter.pseq = filter_taxa(atlas.recode.country.comp.pseq, function(x) mean(x) > 1e-04, TRUE)
-atlas.recode.country.filter.pseq = filter_taxa(mypseq[["atlas.recode.country.pseq" ]], function(x) mean(x) > 1e-04, TRUE)
-
-atlas.recode.country.comp.core.pseq <- core(atlas.recode.country.comp.pseq, detection = 0, prevalence = .5)
-
-
-mypseq[["atlas.recode.country.comp.pseq"]] <- atlas.recode.country.comp.pseq
-mypseq[["atlas.recode.country.comp.filter.pseq"]] <- atlas.recode.country.comp.filter.pseq
-mypseq[["atlas.recode.country.comp.core.pseq"]] <- atlas.recode.country.comp.core.pseq
-mypseq[["atlas.recode.country.filter.pseq"]] <- atlas.recode.country.filter.pseq
-
-
-
-# This removes all of the additional covariates that are not required. 
-sample_data(mypseq[["curat.recode.country.filter.pseq"]])[c(2:3,6:16, 18, 20:95 )] = NULL
-sample_data(mypseq[["curat.recode.country.comp.filter.pseq"]])[c(2:3,6:16, 18, 20:95 )] = NULL
-sample_data(mypseq[["curat.recode.country.comp.pseq"]] )[c(2:3,6:16, 18, 20:95 )] = NULL
-
-
-sample_data(mypseq[["atlas.recode.country.comp.pseq"]])[c(4:6,8:10)] = NULL
-sample_data(mypseq[["atlas.recode.country.comp.filter.pseq"]] )[c(4:6,8:10)] = NULL
-sample_data(mypseq[["atlas.recode.country.filter.pseq"]])[c(4:6,8:10)] = NULL
-
-# This command lists them sample_variables(curat.pseq.comp.filter)
-return(mypseq)
+   # The output is; 
+   # curat.pseq.comp.filte
+   
+   # Relative abundance
+   curat.recode.country.comp.pseq <- microbiome::transform(mypseq[["curat.recode.country.pseq" ]], "compositional")
+   
+   message("Coverting the data to relative abudance")
+   
+   # Filtering based on filter.features for siamcat
+   # siamcat <- filter.features(siamcat, cutoff=1e-04, filter.method = 'abundance', feature.type = 'original')
+   
+   # Filtered such that only OTUs with a mean greater than 1e-05 are kept.
+   curat.recode.country.comp.filter.pseq = filter_taxa(curat.recode.country.comp.pseq , function(x) mean(x) > 1e-04, TRUE)
+   
+   #Relative population frequencies; at 1% compositional abundance threshold:
+   #prevalence(pseq.rel, detection = 1/100, sort = TRUE)
+   
+   
+   
+   # the filtering is really important about relative and counts. Sdome are appropriate. Other are not. 
+   # # Core
+   # keepotu = genefilter_sample(curat.recode.country.comp.pseq, filterfun_sample(topp(0.05)), A=5)
+   # pseq.f <- subset_taxa(curat.recode.country.comp.pseq, keepotu)
+   # pseq.f = subset_taxa(pseq.f, !is.na(Genus))
+   # plot_heatmap(pseq.f, method="PCoA", distance="bray")
+   # 
+   # pseq.filt = subset_taxa(curat.recode.country.comp.pseq, keepotu & !is.na(Genus))
+   # plot_heatmap(pseq.filt, method="PCoA", distance="bray")
+   # 
+   # sum(is.na(otu_table(physeqITS_DADA)))
+   # psmelt(physeqITS_DADA) %>%
+   #   filter(is.na(Abundance))
+   
+   curat.recode.country.comp.core.pseq <- core(curat.recode.country.comp.pseq, detection = 0, prevalence = .5)
+   
+   # Absolute population frequencies (sample count):
+   #prevalence(pseq.rel, detection = 1/100, sort = TRUE, count = TRUE)
+   
+   curat.recode.country.filter.pseq = filter_taxa(mypseq[["curat.recode.country.pseq"]] , function(x) mean(x) > 1e-04, TRUE)
+   curat.recode.country.comp.core.filter.pseq <- core(curat.recode.country.filter.pseq, detection = 0, prevalence = .5)
+   
+   
+   
+   #mypseq[["curat.recode.country.comp.core.filter.nNA.pseq"]] <- curat.recode.country.comp.core.filter.nNA.pseq
+   
+   mypseq[["curat.recode.country.comp.core.filter.pseq"]] <- curat.recode.country.comp.core.filter.pseq
+   mypseq[["curat.recode.country.filter.pseq"]] <- curat.recode.country.filter.pseq
+   mypseq[["curat.recode.country.comp.filter.pseq"]] <- curat.recode.country.comp.filter.pseq
+   mypseq[["curat.recode.country.comp.core.pseq"]] <- curat.recode.country.comp.core.pseq
+   mypseq[["curat.recode.country.comp.pseq"]] <- curat.recode.country.comp.pseq
+   
+   
+   # This removes all of the additional covariates that are not required. 
+   sample_data(mypseq[["curat.recode.country.filter.pseq"]])[c(2:3,6:16, 18, 20:95 )] = NULL
+   sample_data(mypseq[["curat.recode.country.comp.filter.pseq"]])[c(2:3,6:16, 18, 20:95 )] = NULL
+   sample_data(mypseq[["curat.recode.country.comp.pseq"]] )[c(2:3,6:16, 18, 20:95 )] = NULL
+   
+   # This command lists them sample_variables(curat.pseq.comp.filter)
+   return(mypseq)
 }
