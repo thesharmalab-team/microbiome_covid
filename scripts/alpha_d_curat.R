1931b9e3ea483cb22d9af0e8fa961e9c5954dfdf
Giorgia Gentili <giorgiaisabella@gentili.co.uk>
2022-02-02 19:11:22 +0100

    alpha div

--- scripts/alpha_d_curat.R
+++ scripts/alpha_d_curat.R
@@ -5,12 +5,12 @@ alpha_d_curat <- function(curat.pseq) {
   tab <- microbiome::alpha(curat.pseq, index = "shannon")
   # creates Shannon
   curat.meta$Shannon <- tab$diversity_shannon
-  curat.mnd.class <- levels(curat.meta$curat.mnd.class) # get the variables
+  curat.covid.class <- levels(curat.meta$curat.covid.class) # get the variables
   # Make a pairwise list that we want to compare.
-  mnd.pairs <- combn(seq_along(curat.mnd.class), 2, simplify = FALSE, FUN = function(i)curat.mnd.class[i])
-  mnd.pairs <<- list( c("HIGH","LOW"))
+  covid.pairs <- combn(seq_along(curat.covid.class), 2, simplify = FALSE, FUN = function(i)curat.covid.class[i])
+  covid.pairs <<- list( c("HIGH","LOW"))
   # Compare differences in Shannon index between mnd group of the study subjects
-  curat.meta$curat.mnd.class <- factor(curat.meta$curat.mnd.class, levels = c("LOW","HIGH"),  exclude = "NA")
+  curat.meta$curat.covid.class <- factor(curat.meta$curat.covid.class, levels = c("LOW","HIGH"),  exclude = "NA")
 
   return(curat.meta)
 }
