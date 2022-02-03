aeb123961b1ade26e99f9508499ed8fe7be7ff28
Giorgia Gentili <giorgiaisabella@gentili.co.uk>
2022-02-03 12:27:49 +0100

    updates-by_pop

--- scripts/gbm.table.R
+++ scripts/gbm.table.R
@@ -1,19 +1,19 @@
 gbm.table <- function(my.table.data,gbm.mean.levels) {
-  ft <-arrange(my.table.data, Region)
-  ft <-ft %>% dplyr::rename(Country = location) 
+  ft <-arrange(my.table.data, location)
+  #ft <-ft %>% dplyr::rename(Country = location) 
   # creates the means
-  ft.mean <-  summaryBy( . ~ Region, data = ft) %>%
-    add_row()
-  ft <- as_grouped_data(ft, groups = c("Region"))
+  #ft.mean <-  summaryBy( . ~ Region, data = ft) %>%
+  #add_row()
+  #ft <- as_grouped_data(ft, groups = c("Region"))
   
   # Tidy's up the data and renames and then inserts a column with Total 
-  ft.mean <-ft.mean %>% 
-    dplyr::rename(DALY = DALY.mean,Prevalence = Prevalence.mean, Incidence = Incidence.mean) %>% 
-    add_column( Country = "Mean", .after = 1)
+  #ft.mean <-ft.mean %>% 
+  #dplyr::rename(DALY = DALY.mean,Prevalence = Prevalence.mean, Incidence = Incidence.mean) %>% 
+  # add_column( Country = "Mean", .after = 1)
   # Intersts the rows. The number here are critical 
-  ft<- insertRows(ft, gbm.mean.levels, new = ft.mean, rcurrent = FALSE)
+  #ft<- insertRows(ft, gbm.mean.levels, new = ft.mean, rcurrent = FALSE)
   # this removes the country label for the means
-  ft[gbm.mean.levels,1] <- ""
+  #ft[gbm.mean.levels,1] <- ""
   ft <- flextable(ft) %>% 
     footnote( i = 1, j = 3:5,
               value = as_paragraph(
@@ -25,7 +25,7 @@ gbm.table <- function(my.table.data,gbm.mean.levels) {
     valign( valign = "bottom", part = "header") %>% 
     add_header_lines(values = c(tab_title_gbm) ) %>% 
     bold(part = "header") %>%
-    bold(i = gbm.mean.levels) %>%
+    #bold(i = gbm.mean.levels) %>%
     theme_booktabs() %>% 
     autofit() %>% 
     fix_border_issues()
