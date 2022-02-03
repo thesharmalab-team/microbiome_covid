16f29e07480109c0197a6a23e9de9c97b399d7fc
Giorgia Gentili <giorgiaisabella@gentili.co.uk>
2022-02-03 13:01:50 +0100

    Update perma_d_two.R

--- scripts/perma_d_two.R
+++ scripts/perma_d_two.R
@@ -1,32 +1,31 @@
 perma_d_two <- function(mypseq) { # x & y must be either p.low,p.med or p.high
   
   temp.curat <- mypseq[["curat.recode.country.comp.core.pseq" ]] 
-  temp.atlas <- mypseq[["atlas.recode.country.comp.core.pseq" ]] 
   
   # Curat data 
   
-  p.low <- subset_samples(temp.curat, curat.mnd.class == "LOW")
+  p.low <- subset_samples(temp.curat, curat.covid.class == "LOW")
   #p.low.d <- divergence(p.low)
-  p.high <- subset_samples(temp.curat, curat.mnd.class == "HIGH")
+  p.high <- subset_samples(temp.curat, curat.covid.class == "HIGH")
   #p.high.d <- divergence(p.high)
   #p <<- boxplot(list(Low = p.low.d, High = p.high.d))
   
   # Convert to compositional data perma_d(p.low,p.high,"final_highVlow")
-  pseqLvH <- subset_samples(temp.curat, curat.mnd.class == c("LOW","HIGH"))
+  pseqLvH <- subset_samples(temp.curat, curat.covid.class == c("LOW","HIGH"))
   pseq.rel <- microbiome::transform(pseqLvH, "compositional")
   otu <- abundances(pseq.rel)
   meta <- microbiome::meta(pseq.rel)
   
 
   
-  permanova <- adonis(t(otu) ~ curat.mnd.class,  data = meta, permutations=999, method = "bray")
+  permanova <- adonis(t(otu) ~ curat.covid.class,  data = meta, permutations=999, method = "bray")
   # P-value
-  print(as.data.frame(permanova$aov.tab)["curat.mnd.class", "Pr(>F)"])  
+  print(as.data.frame(permanova$aov.tab)["curat.covid.class", "Pr(>F)"])  
   dist <- vegdist(t(otu))
-  anov <- anova(betadisper(dist, meta$curat.mnd.class))
+  anov <- anova(betadisper(dist, meta$curat.covid.class))
   summary(anov)
   tidy(anov)
-  coef <- coefficients(permanova)["curat.mnd.class1",]
+  coef <- coefficients(permanova)["curat.covid.class1",]
   top.coef <- coef[rev(order(abs(coef)))[1:20]]
   par(mar = c(12, 30, 2, 1))
   #KEEP THIS  TO ENSURE THERE IS NOT A MISTAKE IN THE REORDERING BELOW
@@ -44,53 +43,9 @@ perma_d_two <- function(mypseq) { # x & y must be either p.low,p.med or p.high
   permanova.curat <- temp
   
   temp <- NULL
-
-
   
-  # Atlas dataset
-  p.low <- subset_samples(temp.atlas, atlas.mnd.class == "LOW")
-  #p.low.d <- divergence(p.low)
-  p.high <- subset_samples(temp.atlas, atlas.mnd.class == "HIGH")
-  #p.high.d <- divergence(p.high)
-  #p <<- boxplot(list(Low = p.low.d, High = p.high.d))
-  
-  # Convert to compositional data perma_d(p.low,p.high,"final_highVlow")
-  pseqLvH <- subset_samples(temp.atlas, atlas.mnd.class == c("LOW","HIGH"))
-  pseq.rel <- microbiome::transform(pseqLvH, "compositional")
-  otu <- abundances(pseq.rel)
-  meta <- microbiome::meta(pseq.rel)
-
-  
- permanova <- adonis(t(otu) ~ atlas.mnd.class,
-                      data = meta, permutations=999, method = "bray")
-  # P-value
-  print(as.data.frame(permanova$aov.tab)["atlas.mnd.class", "Pr(>F)"])  
-  dist <- vegdist(t(otu))
-  anov <- anova(betadisper(dist, meta$atlas.mnd.class))
-  summary(anov)
-  tidy(anov)
-  coef <- coefficients(permanova)["atlas.mnd.class1",]
-  top.coef <- coef[rev(order(abs(coef)))[1:20]]
-  par(mar = c(12, 30, 2, 1))
-  #KEEP THIS  TO ENSURE THERE IS NOT A MISTAKE IN THE REORDERING BELOW
-  #temp <- TeX("Top Genus")
-  #png(paste("PERMOVA_",title_text,".png", sep = "" ), width = 450, height = 210,  units     = "mm",
-  #res       = 1500)
-  #barplot(sort(top.coef), col = rainbow(20), horiz = T, las = 1, main = temp)
-  #dev.off() 
-  temp <- as.data.frame(top.coef)
-  temp$label <- rownames(temp) 
-  temp$label <- sub("s_*", "", temp[,2])
-  #Add this so the label is in Italics, requires library(ggtext) & library(mdthemes)
-  temp$label <- paste("*",temp$label, "*", sep="")
-  
-  permanova.atlas <- temp
-  
-  mypermanova <- list("permanova.atlas" = permanova.atlas, 
+  mypermanova <- list( 
                  "permanova.curat" = permanova.curat)
-  
 
   return(mypermanova)
-  
-  
 } 
